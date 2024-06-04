91b011703a8dc511eef92d822b1528f98b38f723
Mikael <mikael.1010.mm@gmail.com>
2024-06-04 18:50:20 -0300

    Dados de leituras adicionados

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String nome = "Cristiano Ronaldo";
+        double saldo = 1548.90;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("***********************");
+        System.out.println("Bem vindo " + nome + "\n Seu saldo é " + saldo);
+        System.out.println("***********************");
+
+       String menu=("""
+                    _________________________________
+                    Digite sua opção:
+                    1 - Consultar Saldo
+                    2 - Transferir valor
+                    3 - Receber valor
+                    4 - Sair
+                    _________________________________
+                """);
+
+        Scanner leitura= new Scanner(System.in);
+        int opcao=0;
+
+        while ( opcao != 4) {
+             System.out.println(menu);
+             opcao=leitura.nextInt();
         }
     }
 }
